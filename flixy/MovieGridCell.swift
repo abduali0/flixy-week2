a219a75f67b9f3d61e6a5220772f8d96695c7b65
Abdullah Ali <abduali@iu.edu>
2020-10-03 02:58:33 -0400

    initial commit

---
+++ flixy/MovieGridCell.swift
@@ -0,0 +1,15 @@
+//
+//  MovieGridCell.swift
+//  flixy
+//
+//  Created by Abdullah Ali on 10/3/20.
+//  Copyright © 2020 Abdullah Ali. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
--- flixy/MoviesViewController.swift
+++ flixy/MoviesViewController.swift
@@ -68,6 +68,21 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         }
         task.resume()
     }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?){
+        
+        print("Loading up the details screen")
+        
+        let cell = sender as! UITableViewCell
+        let indexPath = tableView.indexPath(for: cell)!
+        let movie = movies[indexPath.row]
+        
+        let detailsViewController = segue.destination as! MovieDetailsViewController
+        detailsViewController.movie = movie
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        
+    }
 
 
 }
